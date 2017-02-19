d99a481b1fcc573602aa3eeca8df2b61143b1461
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-02-19 10:50:37 -0700

    started home view

--- public/js/controllers/controller.home.js
+++ public/js/controllers/controller.home.js
@@ -5,5 +5,11 @@ angular.module('CharCreator')
   function homeController($scope) {
     var hc = this;
 
-    hc.welcome = 'You have found the amazing Character Creator!!!'
+    hc.welcome = 'You have found the amazing Character Creator!!!';
+
+    hc.sideNav = function() {
+      $('.button-collapse').sideNav({
+        edge: 'right'
+      });
+    }
   }
